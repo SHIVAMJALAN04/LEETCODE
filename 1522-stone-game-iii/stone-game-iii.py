5a9a463856b7c474fe32c51129dd15ad8f14bcb7
SHIVAM JALAN <aec.cse.2024shivamjalan@gmail.com>
2026-08-03 10:32:54 +0530

    Time: 832 ms (53.90%) | Memory: 24 MB (71.59%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.py
+++ 1522-stone-game-iii/stone-game-iii.py
@@ -3,11 +3,9 @@ class Solution:
         n = len(stoneValue)
 
         dp = [0] * (n + 1)
-
         for i in range(n - 1, -1, -1):
             dp[i] = float("-inf")
             take = 0
-
             for k in range(3):
                 if i + k < n:
                     take += stoneValue[i + k]
